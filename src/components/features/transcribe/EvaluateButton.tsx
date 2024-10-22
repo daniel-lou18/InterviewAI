9b85a9cac043c03f647734622c2b198f8e72527d
daniel-lou18 <danielderudder@hotmail.com>
2024-10-22 19:12:46 +0200

    Correct conditional rendering of EvaluateButton

--- src/components/features/transcribe/EvaluateButton.tsx
+++ src/components/features/transcribe/EvaluateButton.tsx
@@ -11,19 +11,29 @@ export default function EvaluateButton({ isPending }: EvaluateButtonProps) {
   const { transcription, evaluation } = useInterview();
   const text = transcription ? transcription.text : "";
 
+  if (!isPending && !!evaluation) {
+    return (
+      <div className="flex items-center text-green-600 text-sm font-semibold">
+        <CheckedButtonContent />
+      </div>
+    );
+  }
+
   return (
-    <Button
-      type="submit"
-      size="sm"
-      disabled={!text || (!isPending && !!evaluation)}
-      className=" bg-blue-500 hover:bg-blue-600 transition-colors duration-300"
-    >
-      <LoaderWrapper
-        isLoading={isPending}
-        LoadingIndicator={LoadingButtonContent}
+    <>
+      <Button
+        type="submit"
+        size="sm"
+        disabled={!text}
+        className="bg-blue-500 hover:bg-blue-600 transition-colors duration-300"
       >
-        {evaluation ? <CheckedButtonContent /> : <EvaluateButtonContent />}
-      </LoaderWrapper>
-    </Button>
+        <LoaderWrapper
+          isLoading={isPending}
+          LoadingIndicator={LoadingButtonContent}
+        >
+          <EvaluateButtonContent />
+        </LoaderWrapper>
+      </Button>
+    </>
   );
 }
